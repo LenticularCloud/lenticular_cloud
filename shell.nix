1bf474045a2105532105721feaa6c71827e91ece
TuxCoder <git@o-g.at>
2022-02-04 23:02:27 +0100

    better packaging

--- shell.nix
+++ shell.nix
@@ -7,6 +7,6 @@ let
 in
 pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = settings.nativeBuildInputs ++ settings.testBuildInputs ++ [ pkgs.nodePackages.npm pkgs.nodejs ];
+    nativeBuildInputs = settings.nativeBuildInputs ++ settings.testBuildInputs ++ [ pkgs.nodePackages.npm pkgs.nodejs python.pkgs.build ];
 }
 
